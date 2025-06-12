f8eff8148642e1b6fa896854720cf0e06274b949
Amaan Arman <amaanarman99@gmail.com>
2025-06-12 18:50:25 +0500

    first commit DVMS

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DVMS.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult companies()
+        {
+            return View();
+        }
+        public IActionResult employee()
+        {
+            return View();
+        }
+        public IActionResult inviteguest()
+        {
+            return View();
+        }
+        public IActionResult trackguest()
+        {
+            return View();
+        }
+        public IActionResult trackvisitor()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
